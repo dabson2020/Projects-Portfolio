8107202998c2f5eef42e9a5a6e5ef3e9e0f2d483
Adeola Aberefa Ayandeyi <adeolaayandeyi@gmail.com>
2024-04-28 18:15:56 -0600

    Update analysis.py

--- Data Analysis and Data Engineering Projects/Data Analysis with Python and SQL/analysis.py
+++ Data Analysis and Data Engineering Projects/Data Analysis with Python and SQL/analysis.py
@@ -1,5 +1,5 @@
-# %% [markdown]
-# This project loads data with kaggle API, cleans the data and load the data into SQL Server for further analysis
+
+# This project loads data with Kaggle API, cleans the data, and load the data into SQL Server for further analysis
 
 # %%
 import pandas as pd
@@ -9,10 +9,10 @@ import seaborn as sns
 import kaggle
 import zipfile
 
-# %% [markdown]
+
 # We are loading the data from kaggle using kaggle api
 
-# %%
+
 def load_data():
     # Load data
     !kaggle datasets download ankitbansal06/retail-orders
@@ -23,23 +23,22 @@ def load_data():
         return df
 
 
-# %% [markdown]
-# Extracting thr zip file of the datsets downloaded from kaggle
 
-# %%
+# Extracting the zip file of the datsets downloaded from kaggle
+
+
 df = load_data()
 
 
-# %% [markdown]
-# Data Cleaning
+
+### Data Cleaning
 # - Replace missing values  
-#     - Replace NaN, 'Not Available' and 'unknown' in Ship_mode column
+#   - Replace NaN, 'Not Available' and 'unknown' in Ship_mode column
 # - Convert column names to lowercase and replace whitespace with underscore
 # - Compute the sales price and discount and profit
 # - Drop the cost_price,list_price and discount_percent
 # - Convert order_date into datetime data type
 
-# %%
 def data_cleaning(df):
     # replace missing values
     df['Ship Mode'] = df['Ship Mode'].fillna('Standard Class')
@@ -62,27 +61,23 @@ def data_cleaning(df):
    
     return df
 
-# %%
+
 data=data_cleaning(df)
 
-# %%
-data.info()
-
-# %% [markdown]
 # Load data into MS SQL Server using msqlalchemy
 
-# %%
+
 import sqlalchemy as sqal
 engine = sqal.create_engine('mssql://ADEOLA\SQLEXPRESS/retail?driver=ODBC+DRIVER+17+for+SQL+Server')
 conn=engine.connect()
-# the data will be loaded into the tables you already created in MMSQL
+# The data will be loaded into the tables you already created in MMSQL
 
 data.to_sql('orders',conn,if_exists='append',index=False)
 
-# I you want the table to be created in the database, you can use the following code
+# If you want the table to be created in the database, you can use the following code
 #data.to_sql('orders',conn,if_exists='replace',index=False)
 
-# %%
+
 
 
 
